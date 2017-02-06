f3b8f54df3363183eacacebb8e0c76ed4d2a71f3
Wichard Riezebos <wichardriezebos@gmail.com>
2017-02-06 14:18:35 +0100

    added factories for di-frameworks

---
+++ AntPathMatching/IAntDirectoryFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace AntPathMatching
+{
+    /// <summary>
+    /// Represents an interface to create <see cref="IAntDirectory"/> instances. Mainly used for DI frameworks.
+    /// </summary>
+    public interface IAntDirectoryFactory
+    {
+        /// <summary>
+        /// Initializes a new <see cref="IAntDirectory"/>.
+        /// </summary>
+        /// <param name="ant">Ant pattern used for directory-searching.</param>
+        IAntDirectory CreateNew(IAnt ant);
+    }
+}
---
+++ AntPathMatching/IAntFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace AntPathMatching
+{
+    /// <summary>
+    /// Represents an interface to create <see cref="IAnt"/> instances. Mainly used for DI frameworks.
+    /// </summary>
+    public interface IAntFactory
+    {
+        /// <summary>
+        /// Creates a new <see cref="IAnt"/>.
+        /// </summary>
+        /// <param name="pattern">Ant-style pattern.</param>
+        IAnt CreateNew(string pattern);
+    }
+}
