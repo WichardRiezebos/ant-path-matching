ab29489949bd9ac945ddad5df5826b484a02c559
Boris Modylevsky <boris.modylevsky@citi.com>
2018-09-03 23:24:47 +0300

    inject IFileSystem into AntDirectory

--- AntPathMatching.Tests/AntDirectoryTests.cs
+++ AntPathMatching.Tests/AntDirectoryTests.cs
@@ -1,50 +1,126 @@
 ﻿using NUnit.Framework;
 using System;
+using System.IO.Abstractions.TestingHelpers;
 using System.Linq;
+using FluentAssertions;
 
 namespace AntPathMatching
 {
     [TestFixture]
     public class AntDirectoryTests
     {
-        [TestCase("/assets/**/*.txt", true)]
-        [TestCase("/assets/**/*.xml", false)]
-        [TestCase("/assets/dir1/*/file?.txt", true)]
-        [TestCase("/assets/dir1/*/file???.txt", false)]
-        [TestCase("/assets/**/*.*", true)]
-        public void SearchRecursively_FromTable_ReturnsExpected(
-            string pattern,
-            bool expected)
+        [Test]
+        public void SearchRecursively_RecursiveFilesExist_ReturnsTwoFiles()
         {
-            var ant = new AntDirectory(new Ant(pattern));
-            var match = ant.SearchRecursively(AppDomain.CurrentDomain.BaseDirectory);
+            // Arrange
+            var mockFileSystem = CreateMockFileSystem();
 
-            Assert.That(match, expected ? Is.Not.Empty : Is.Empty);
+            var ant = new AntDirectory(new Ant("/assets/**/*.txt"), mockFileSystem);
+
+            // Act
+            var files = ant.SearchRecursively(@"C:\").ToList();
+
+            // Assert
+            files.Should().HaveCount(2);
+            files.Should().Contain(f => f == @"assets\dir1\dir1_1\file1.txt");
+            files.Should().Contain(f => f == @"assets\dir1\dir1_1\file2.txt");
+        }
+
+        [Test]
+        public void SearchRecursively_RecursiveFilesDoNotExist_ReturnsEmpty()
+        {
+            // Arrange
+            var mockFileSystem = CreateMockFileSystem();
+
+            var ant = new AntDirectory(new Ant("/assets/**/*.xml"), mockFileSystem);
+
+            // Act
+            var files = ant.SearchRecursively(@"C:\");
+
+            // Assert
+            files.Should().BeEmpty();
+        }
+
+        [Test]
+        public void SearchRecursively_PatternWithQuestionMark_ReturnsTwoFiles()
+        {
+            // Arrange
+            var mockFileSystem = CreateMockFileSystem();
+
+            var ant = new AntDirectory(new Ant("/assets/dir1/*/file?.txt"), mockFileSystem);
+
+            // Act
+            var files = ant.SearchRecursively(@"C:\").ToList();
+
+            // Assert
+            files.Should().HaveCount(2);
+            files.Should().Contain(f => f == @"assets\dir1\dir1_1\file1.txt");
+            files.Should().Contain(f => f == @"assets\dir1\dir1_1\file2.txt");
+        }
+
+        [Test]
+        public void SearchRecursively_PatternWithThreeQuestionMarks_ReturnsEmpty()
+        {
+            // Arrange
+            var mockFileSystem = CreateMockFileSystem();
+
+            var ant = new AntDirectory(new Ant("/assets/dir1/*/file???.txt"), mockFileSystem);
+
+            // Act
+            var files = ant.SearchRecursively(@"C:\");
+
+            // Assert
+            files.Should().BeEmpty();
+        }
+
+        [Test]
+        public void SearchRecursively_PatternRecursiveWithWildcard_ReturnsThreeFiles()
+        {
+            // Arrange
+            var mockFileSystem = CreateMockFileSystem();
+
+            var ant = new AntDirectory(new Ant("/assets/**/*.*"), mockFileSystem);
+
+            // Act
+            var files = ant.SearchRecursively(@"C:\").ToList();
+
+            // Assert
+            files.Should().HaveCount(3);
+            files.Should().Contain(f => f == @"assets\dir1\dir1_1\file1.txt");
+            files.Should().Contain(f => f == @"assets\dir1\dir1_1\file2.txt");
+            files.Should().Contain(f => f == @"assets\dir2\file2.zip");
+        }
+
+        private static MockFileSystem CreateMockFileSystem()
+        {
+            var mockFileSystem = new MockFileSystem();
+            mockFileSystem.AddFile(@"C:\assets\dir1\dir1_1\file1.txt", new MockFileData("data"));
+            mockFileSystem.AddFile(@"C:\assets\dir1\dir1_1\file2.txt", new MockFileData("data"));
+            mockFileSystem.AddFile(@"C:\assets\dir2\file2.zip", new MockFileData("data"));
+            return mockFileSystem;
         }
 
         [TestCase(true), TestCase(false)]
         public void SearchRecursively_WithDirectoryPathSetting_ReturnsExpectedValue(
             bool includeDirectoryPath)
         {
-            var dirPath = AppDomain.CurrentDomain.BaseDirectory;
-
-            var ant = new AntDirectory(new Ant("/assets/dir1/*/file?.txt"));
-            var match = ant.SearchRecursively(dirPath, includeDirectoryPath);
+            var ant = new AntDirectory(new Ant("/assets/dir1/*/file?.txt"), CreateMockFileSystem());
+            var match = ant.SearchRecursively(@"C:\", includeDirectoryPath);
 
             if (includeDirectoryPath)
             {
-                Assert.That(match.FirstOrDefault(), Does.Contain(dirPath));
+                match.First().Should().StartWith(@"C:\");
             }
             else
             {
-                Assert.That(match.FirstOrDefault(), Does.Not.Contain(dirPath));
+                match.First().Should().NotStartWith(@"C:\");
             }
         }
 
         [Test]
         public void SearchRecursively_WhenDirectoryNotFound_DoesNotThrow()
         {
-            var ant = new AntDirectory(new Ant("*.txt"));
+            var ant = new AntDirectory(new Ant("*.txt"), new MockFileSystem());
 
             Assert.DoesNotThrow(() => ant.SearchRecursively(@"C:\Octopus\Applications\production\Containers").ToList());
         }
